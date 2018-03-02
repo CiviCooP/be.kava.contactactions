f3252e983657a8c4903c447e9d11effdf6da56d9
Alain Benbassat <alain@businessandcode.eu>
2018-03-02 11:37:55 +0100

    first version

---
+++ contactactions.php
@@ -0,0 +1,178 @@
+<?php
+
+require_once 'contactactions.civix.php';
+use CRM_Contactactions_ExtensionUtil as E;
+
+
+function contactactions_civicrm_summaryActions(&$actions, $contactID) {
+  $formattedAddress = '';
+
+  // lookup the primary address
+  $params = array(
+    'id' => $contactID,
+    'is_primary' => 1,
+    'sequential' => 1,
+  );
+  $addresses = civicrm_api3('Address', 'get', $params);
+
+  if ($addresses['count'] > 0) {
+    $formattedAddress = $addresses[values][0]['street_address'];
+    $formattedAddress .= $addresses[values][0]['supplemental_address_1'];
+  }
+
+  $actions['otherActions']['printAddressLabel'] = array(
+    'title' => 'Print adres',
+    'weight' => 999,
+    'ref' => 'print-address-label',
+    'key' => 'printAddressLabel',
+    'href' => 'http://dit-is-een-test.be?addr=' . $formattedAddress,
+  );
+}
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function contactactions_civicrm_config(&$config) {
+  _contactactions_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function contactactions_civicrm_xmlMenu(&$files) {
+  _contactactions_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function contactactions_civicrm_install() {
+  _contactactions_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
+ */
+function contactactions_civicrm_postInstall() {
+  _contactactions_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function contactactions_civicrm_uninstall() {
+  _contactactions_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function contactactions_civicrm_enable() {
+  _contactactions_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function contactactions_civicrm_disable() {
+  _contactactions_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function contactactions_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _contactactions_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function contactactions_civicrm_managed(&$entities) {
+  _contactactions_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function contactactions_civicrm_caseTypes(&$caseTypes) {
+  _contactactions_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+ */
+function contactactions_civicrm_angularModules(&$angularModules) {
+  _contactactions_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function contactactions_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _contactactions_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
+ *
+function contactactions_civicrm_preProcess($formName, &$form) {
+
+} // */
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_navigationMenu
+ *
+function contactactions_civicrm_navigationMenu(&$menu) {
+  _contactactions_civix_insert_navigation_menu($menu, NULL, array(
+    'label' => E::ts('The Page'),
+    'name' => 'the_page',
+    'url' => 'civicrm/the-page',
+    'permission' => 'access CiviReport,access CiviContribute',
+    'operator' => 'OR',
+    'separator' => 0,
+  ));
+  _contactactions_civix_navigationMenu($menu);
+} // */
